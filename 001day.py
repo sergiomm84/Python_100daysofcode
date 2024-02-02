115b766a6512ea976df3ebe7577f93451b7cb60e
Sergio Monterrubio <98435000+sergiomm84@users.noreply.github.com>
2024-02-01 19:01:34 -0500

    Add files via upload

---
+++ 001day.py
@@ -0,0 +1,16 @@
+print("hello world"+"2")
+print("Second line")
+print("""
+This is a multiline string
+.
+..
+...
+....
+""")
+print("""Sergio
+February 1st 2024
+I am signing up for Replit's 100 days of Python challenge!
+It's cool.
+Feeling ok. 🥳
+https://replit.com/@sergiomm84/
+""")
