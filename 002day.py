898968037a852996ea16981a66cbe30d39dbbe64
sergiomm84 <sergiomonterrubio@outlook.com>
2024-02-01 19:54:46 -0500

    second day

---
+++ 002day.py
@@ -0,0 +1,23 @@
+
+#myName=input("What's your name?")
+#myAge=input("How old are you?")
+#print("Got it")
+#print("Last question")
+#myAnswer=input("Do you like Pyton?")
+#print("Nice.")
+#print("""Data collected:
+#Name:"+myName+"
+#Age:"+myAge+"
+#Answer:"+myAnswer+
+#""")
+
+print("Hi. Getting to know you!")
+myName = input("What is your name?")
+myColor = input ("What is your favorite color?")
+mySong = input("What is your favorite song?")
+WhereAreYou = input("Where do you live?")
+print("Hi, " +myName)
+print("Your favorite color is "+myColor)
+print("Your favorite song is "+mySong)
+print("You live in "+WhereAreYou)
+print("So. Farewell. Have a nice day!")
