dab5879e970ec4ea4c723a4cf7f46a57d3aa7166
sergiomm84 <sergiomonterrubio@outlook.com>
2024-02-07 21:29:44 -0500

    Ejercicio 2 - Introduccion HTML , CV en HTML

---
+++ 007day.py
@@ -0,0 +1,16 @@
+print("fake fan finder")
+print("/*/*/*/*/*/*/*/*/*/*/*/*")
+
+myAnime=input("Do you like anime?")
+if myAnime=="yes":
+  myName=input("Oh really?! Name me the anime you like the most?")
+  if myName=="pokémon":
+    myCharacter=input("Oh really?! Name me the main character?")
+    if myCharacter=="ash":
+      myJob=input("Amazing! You're a fan!")
+    else:
+      print("Wrong")
+  elif myName=="pokemon":
+    print("Mmmm, not sure, you didn't write the accent")
+else:
+  print("then bye")
